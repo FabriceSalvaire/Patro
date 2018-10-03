ad0aa460395551fa79e544145f8e10f784761fb4
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2018-10-03 16:53:20 +0200

    non-public-submodule

--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,7 @@
 source /opt/python-virtual-env/py36/bin/activate
 append_to_python_path_if_not ${PWD}
 append_to_python_path_if_not ${PWD}/tools
+
+if [ -e ${PWD}/non-public-submodule/setenv.sh ]; then
+  source ${PWD}/non-public-submodule/setenv.sh
+fi
