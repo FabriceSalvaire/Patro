1df2fd0ec26e6a6239ede742c5d36c2fe4fc3e97
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2018-12-30 17:36:49 +0100

    implemented DXF import

---
+++ Patro/FileFormat/Dxf/Importer.py
@@ -0,0 +1,131 @@
+####################################################################################################
+#
+# Patro - A Python library to make patterns for fashion design
+# Copyright (C) 2017 Fabrice Salvaire
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+####################################################################################################
+
+####################################################################################################
+
+import ezdxf
+
+from Patro.GeometryEngine.Bezier import CubicSpline2D
+from Patro.GeometryEngine.Conic import Circle2D, Conic2D, Interval
+from Patro.GeometryEngine.Segment import Segment2D
+from Patro.GeometryEngine.Vector import Vector2D
+
+from .Polyline import Polyline
+
+####################################################################################################
+
+class DxfImporter:
+
+    ##############################################
+
+    def __init__(self, path):
+
+        path = str(path)
+        self._drawing = ezdxf.readfile(path)
+        self._model_space = self._drawing.modelspace()
+        self._items = []
+        self._read()
+
+    ##############################################
+
+    @staticmethod
+    def _to_vector(point):
+        return Vector2D(point[:2])
+
+    @classmethod
+    def _to_vectors(cls, points):
+        return [cls._to_vector(x) for x in points]
+
+    ##############################################
+
+    def _add(self, item):
+        self._items.append(item)
+
+    ##############################################
+
+    def _read(self):
+
+        for item in self._model_space:
+            dxf_type = item.dxftype()
+            if dxf_type == 'LINE':
+                self._on_line(item)
+            elif dxf_type in ('CIRCLE', 'ARC'):
+                self._on_circle(item, dxf_type == 'ARC')
+            elif dxf_type == 'ELLIPSE':
+                self._on_ellipse(item)
+            elif dxf_type == 'LWPOLYLINE':
+                self._on_polyline(item)
+            elif dxf_type == 'SPLINE':
+                self._on_spline(item)
+
+    ##############################################
+
+    def _on_line(self, item):
+        segment = Segment2D(*self._to_vectors((item.dxf.start, item.dxf.end)))
+        self._add(segment)
+
+    ##############################################
+
+    def _on_circle(self, item, is_arc):
+
+        if is_arc:
+            # Fixme:
+            # domain = Interval(item.dxf.start_angle, item.dxf.end_angle)
+            domain = None
+        else:
+            domain = None
+        circle = Circle2D(self._to_vector(item.dxf.center), item.dxf.radius, domain=domain)
+        self._add(circle)
+
+    ##############################################
+
+    def _on_circle(self, item, is_arc):
+
+        # domain = (item.dxf.start_param, item.dxf.end_param
+        domain = None
+        major_axis = self._to_vector(item.dxf.major_axis)
+        minor_axis = major_axis * item.dxf.ratio
+        # ellipse = Circle2D(self._to_vector(item.dxf.center), major_axis, minor_axis, domain=domain)
+        # self._add(ellipse)
+
+    ##############################################
+
+    def _on_polyline(self, item):
+
+        polyline = Polyline(item.closed)
+        for x, y, s, e, b in item.get_points():
+            polyline.add(x, y, b)
+
+            # for item in polyline:
+            #     if item.bulge:
+            #         # item.demi_chord, item.bulge_radius, item.angle
+            #         pass
+            #     else:
+            #         pass
+
+    ##############################################
+
+    def _on_spline(self, item):
+
+        # print(item.closed)
+        with item.edit_data() as data:
+            points = self._to_vectors(data.control_points)
+        spline = CubicSpline2D(points) # Fixme: cubic ???
+        self._add(spline)
---
+++ Patro/FileFormat/Dxf/Polyline.py
@@ -0,0 +1,164 @@
+####################################################################################################
+#
+# Patro - A Python library to make patterns for fashion design
+# Copyright (C) 2017 Fabrice Salvaire
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+####################################################################################################
+
+####################################################################################################
+
+import math
+
+####################################################################################################
+
+class PolylineVertex:
+
+    # sagitta of a circular arc is the distance from the center of the arc to the center of its base.
+    #
+    # bulge = sagitta / half the length of the chord
+    #   a bulge value of 1 defines a semicircle
+    #
+    # The sign of the bulge value defines the side of the bulge:
+    #     positive value (> 0): bulge is right of line (count clockwise)
+    #     negative value (< 0): bulge is left of line (clockwise)
+    #     0 = no bulge
+    # points = item.get_points(format='xyseb') # ???
+
+    # r = (s**2 + l**2) / (2*s)
+    # b = s / l
+    #
+    # r = ((b*l)**2 + l**2) / (2*b*l)
+    #   = l/2 * (b + 1/b)
+
+    ##############################################
+
+    def __init__(self, polyline, index, x, y, bulge=0):
+
+        self._polyline = polyline
+        self._index = index
+        self._x = x
+        self._y = y
+        self._bulge = bulge
+
+    ##############################################
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def bulge(self):
+        return self._bulge
+
+    ##############################################
+
+    @property
+    def prev(self):
+        if self._index == 0 and not self._polyline.closed:
+                return None
+        return self._polyline[self._index -1]
+
+    ##############################################
+
+    @property
+    def next(self):
+        if self._polyline.is_last_index(self._index):
+            if self._polyline.closed:
+                return self._polyline[0]
+            else:
+                return None
+        return self._polyline[self._index +1]
+
+    ##############################################
+
+    def __str__(self):
+        return '({0._x:5.2f} {0._y:5.2f} {0._bulge:5.2f})'.format(self)
+
+    ##############################################
+
+    @property
+    def demi_chord(self):
+        next_vertex = self.next
+        return math.sqrt((next_vertex.x - self._x)**2 + (next_vertex.y - self._y)**2)
+
+    ##############################################
+
+    @property
+    def sagitta(self):
+        return self.bulge * self.demi_chord
+
+    ##############################################
+
+    @property
+    def bulge_radius(self):
+        if self._bulge == 0:
+            return 0
+        else:
+            return self.demi_chord/2 * (self._bulge + 1/self._bulge)
+
+    ##############################################
+
+    @property
+    def angle(self):
+        if self._bulge == 0:
+            return 0
+        else:
+            return math.degrees(2 * math.atan(self.bulge_radius/self.demi_chord - self._bulge))
+
+####################################################################################################
+
+class Polyline:
+
+    ##############################################
+
+    def __init__(self, closed=False):
+        self._vertices = []
+        self._closed = closed
+
+    ##############################################
+
+    @property
+    def closed(self):
+        return self._closed
+
+    ##############################################
+
+    def __len__(self):
+        return len(self._vertices)
+
+    def __iter__(self):
+        return iter(self._vertices)
+
+    def __getitem__(self, index):
+        return self._vertices[index]
+
+    def is_last_index(self, index):
+        return index == len(self) - 1
+
+    ##############################################
+
+    def add(self, *args, **kwargs):
+        vertex = PolylineVertex(self, len(self), *args, **kwargs)
+        self._vertices.append(vertex)
+
+    ##############################################
+
+    def __str__(self):
+        return ' '.join(['{:5.2f}'.format(vertex) for vertex in self])
