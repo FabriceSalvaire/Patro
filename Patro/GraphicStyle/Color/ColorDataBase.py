9a9a55265d41e185dd3d9cb0533768197d631089
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2019-01-22 19:53:42 +0100

    improved color

--- Patro/GraphicStyle/Color/ColorDataBase.py
+++ Patro/GraphicStyle/Color/ColorDataBase.py
@@ -28,6 +28,10 @@ __all__ = ['Color', 'ColorDataBase']
 
 ####################################################################################################
 
+import colorsys
+
+####################################################################################################
+
 class Color:
 
     """Class to define a colour
@@ -59,19 +63,32 @@ class Color:
         if number_of_args == 1:
             color = args[0]
             if isinstance(str, Color):
-                self._red = color.red
-                self._green = color.green
-                self._blue = color.blue
+                self._red = color.red_float
+                self._green = color.green_float
+                self._blue = color.blue_float
             else:
                 rgb = str(color)
                 if not rgb.startswith('#'):
                     raise ValueError('Invalid color {}'.format(rgb))
                 rgb = rgb[1:]
-                self._red = int(rgb[:2], 16)
-                self._green = int(rgb[2:4], 16)
-                self._blue = int(rgb[-2:], 16)
+                red, green, blue = rgb[:2], rgb[2:4], rgb[-2:]
+                self._red, self._green, self._blue = [self._to_float(int(x, 16))
+                                                      for x in (red, green, blue)]
         elif number_of_args == 3:
-            self._red, self._green, self._blue = [int(arg) for arg in args]
+            self._red, self._green, self._blue = [self._check_value(arg) for arg in args]
+        else:
+            self._red, self._green, self._blue = 0
+
+        if 'hue' in kwargs:
+            if 'light' in kwargs:
+                hue, light, saturation = [kwargs[x] for x in ('hue', 'light', 'saturation')]
+                red, green, blue = colorsys.hls_to_rgb(hue, light, saturation)
+            elif 'value' in kwargs:
+                hue, value, saturation = [kwargs[x] for x in ('hue', 'value', 'saturation')]
+                red, green, blue = colorsys.hsv_to_rgb(hue, saturation, value)
+            else:
+                raise ValueError('Missing color parameter')
+            self._red, self._green, self._blue = [self._check_value(x) for x in red, green, blue]
 
         # self._name = kwargs.get('name', None)
         if 'name' in kwargs:
@@ -96,40 +113,78 @@ class Color:
 
     ##############################################
 
+    @staticmethod
+    def _to_int(x):
+        return int(x*255)
+
+    @staticmethod
+    def _to_float(x):
+        return x/255
+
+    ##############################################
+
     def _check_value(self, value):
+
         if isinstance(value, int):
             if 0 <= value <= 255:
-                return value
+                return self._to_float(value)
+
         if isinstance(value, float):
             if 0 <= value <= 1:
-                return int(value * 255)
+                # return int(value * 255)
+                return value # keep float
+
         raise ValueError('Invalid colour {}'.format(value))
 
     ##############################################
 
     @property
-    def red(self):
+    def red_float(self):
         return self._red
+
+    @property
+    def red(self):
+        return self._to_int(self._red)
 
     @red.setter
     def red(self, value):
         self._red = self._check_value(value)
 
     @property
-    def green(self):
+    def green_float(self):
         return self._green
+
+    @property
+    def green(self):
+        return self._to_int(self._green)
 
     @green.setter
     def green(self, value):
         self._green = self._check_value(value)
 
     @property
-    def blue(self):
+    def blue_float(self):
         return self._blue
+
+    @property
+    def blue(self):
+        return self._to_int(self._blue)
 
     @blue.setter
     def blue(self, value):
         self._blue = self._check_value(value)
+
+    ##############################################
+
+    # note hue and saturation is ambiguous
+
+    @property
+    def hls(self):
+        return colorsys.rgb_to_hls(self._red, self._green, self._blue)
+
+    @property
+    def hsv(self):
+        return colorsys.rgb_to_hsv(self._red, self._green, self._blue)
 
     ##############################################
 
