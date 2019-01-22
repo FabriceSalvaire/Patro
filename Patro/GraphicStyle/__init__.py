c30bb0472f2a66459ff3c89fa590886687ed3b1d
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2019-01-22 19:53:53 +0100

    added cap and join style

--- Patro/GraphicStyle/__init__.py
+++ Patro/GraphicStyle/__init__.py
@@ -41,9 +41,38 @@ class StrokeStyle(Enum):
 
      """Enum class to define stroke styles"""
 
-     NoPen = auto()
+     NoPen = auto() # Inivisble ?
      SolidLine = auto()
      DashLine =	auto()
      DotLine = auto()
      DashDotLine = auto()
      DashDotDotLine = auto()
+     # Custom
+
+####################################################################################################
+
+class CapStyle(Enum):
+
+     """Enum class to define cap styles"""
+
+     #: a square line end that does not cover the end point of the line
+     FlatCap = auto()
+     #: a square line end that covers the end point and extends beyond it by half the line width
+     SquareCap = auto()
+     #: a rounded line end.
+     roundCap = auto()
+
+####################################################################################################
+
+class JoinStyle(Enum):
+
+     """Enum class to define join styles"""
+
+     #! The outer edges of the lines are extended to meet at an angle, and this area is filled.
+     MiterJoin = auto()
+     #: The triangular notch between the two lines is filled.
+     BevelJoin = auto()
+     #: A circular arc between the two lines is filled.
+     RoundJoin = auto()
+     #: A miter join corresponding to the definition of a miter join in the SVG 1.2 Tiny specification.
+     SvgMiterJoin = auto()
