8d9bc0450fd1e0299774c6955f91256535c5d089
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2019-01-25 18:12:57 +0100

    setenv

--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,7 @@
-source /opt/python-virtual-env/py37/bin/activate
+py37
+
 append_to_python_path_if_not ${PWD}
+append_to_python_path_if_not ${PWD}/examples
 append_to_python_path_if_not ${PWD}/tools
 
 if [ -e ${PWD}/non-public-submodule/setenv.sh ]; then
