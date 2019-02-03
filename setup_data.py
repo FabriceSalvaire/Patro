2e19987e35eb3479cc7aa0e910fbf7952194cf90
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2019-02-03 17:25:16 +0100

    Updated license to AGPL v3

--- setup_data.py
+++ setup_data.py
@@ -4,17 +4,17 @@
 # Copyright (C) 2018 Fabrice Salvaire
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 ####################################################################################################
 
@@ -78,7 +78,7 @@ setup_dict = dict(
     author='Fabrice Salvaire',
     author_email='fabrice.salvaire@orange.fr',
     description='A Python library to make pattern for fashion design',
-    license='GPLv3',
+    license='AGPLv3',
     keywords= 'pattern making drafting digitising digitizing sewing fashion design garment cloth valentina seamly svg latex tikz geometry engine cad',
     url='https://github.com/FabriceSalvaire/Patro',
     long_description=long_description,
